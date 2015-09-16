d2995852162d01d16673e45a0863ba7d9b57e161
kajatrijsburg <kajatrijsburg@gmail.com>
2015-09-16 16:44:02 +0200

    chats?

--- JS.js
+++ JS.js
@@ -5,6 +5,7 @@ function display_talk_to_me(){
 	document.getElementById("custom_text").innerHTML= "make this display a chat";
 }
 function display_chat_rooms(){
+	<script id="cid0020000103405879154" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 250px;height: 350px;">{"handle":"soul-animetv","arch":"js","styles":{"a":"004400","b":100,"c":"FFFFFF","d":"FFFFFF","k":"004400","l":"004400","m":"004400","n":"FFFFFF","p":"14.31","q":"004400","r":100,"cnrs":"0.35","fwtickm":1}}</script>
 	document.getElementById("custom_text").innerHTML = "make this display all chats";
 }
 function display_friends(){
