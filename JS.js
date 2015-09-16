fa6a516070762db45186ae692382b5caa8eb1110
kajatrijsburg <kajatrijsburg@gmail.com>
2015-09-16 17:33:14 +0200

    huge rewrite pt js

--- JS.js
+++ JS.js
@@ -1,13 +1,11 @@
-function display_about(){
-	document.getElementById("custom_text").innerHTML = "I'm cool";
+function hide_all(){
+	var categories["about_me", "talk_to_me", "chat_rooms", "friends"];
+	for(var i=0; i < categories.length; i++){
+		document.getElementById(categories[i]).style.display='none';
+	}
 }
-function display_talk_to_me(){
-	document.getElementById("custom_text").innerHTML= "make this display a chat";
-}
-function display_chat_rooms(){
-	document.getElementById("custom_text").innerHTML = <script id="cid0020000103405879154" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 250px;height: 350px;">{"handle":"soul-animetv","arch":"js","styles":{"a":"004400","b":100,"c":"FFFFFF","d":"FFFFFF","k":"004400","l":"004400","m":"004400","n":"FFFFFF","p":"14.31","q":"004400","r":100,"cnrs":"0.35","fwtickm":1}}</script>
-	//document.getElementById("custom_text").innerHTML = "I'm looking for more ^-^. If you know any, hit me up!";
-}
-function display_friends(){
-	document.getElementById("custom_text").innerHTML = "make this display all friends";
+
+function display_category(category){
+	hide_all();
+	document.getElementById(category).style.display='block'
 }
