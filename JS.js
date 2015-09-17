a8719cafc9c200bf65570a811c5ee9e974a46d7b
kajatrijsburg <kajatrijsburg@gmail.com>
2015-09-17 09:36:12 +0200

    fix for firefox

--- JS.js
+++ JS.js
@@ -1,12 +1,12 @@
 function hide_all(){
 	var categories = ['about_me', 'talk_to_me', 'chat_rooms', 'friends', 'welcome'];
 	for(var i=0; i < categories.length; i++){
-		document.getElementById(categories[i]).style.display='none';
+		document.getElementById(categories[i]).style.visibility = 'hidden';
 	}
 }
 
 function display_category(category){
 	hide_all();
 	var i = category;
-	document.getElementById(i).style.display='block'
+	document.getElementById(i).style.visibility = 'hidden';
 }
